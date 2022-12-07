d5badd36f5fb17ce044b531599d2066791146428
RodrigoFA <rflores@alumno.ipn.mx>
2022-12-07 16:07:57 -0600

    Listo eliminar tareas, falta editar

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -3,16 +3,35 @@ import {useEffect, useState} from 'react'
 import {Button, Card, CardContent, Typography} from '@mui/material'
 
 function TaskList() {
-    const [tasks, setTasks] = useState([])
+    const [tasks, setTasks] = useState([]);
+
     const loadTasks= async()=>{
-        const response=await fetch('http://localhost:4000/readelements');
-        const data= await response.json();
-        setTasks(data)
-        // console.log(data);
+        try {
+            const response=await fetch('http://localhost:4000/readelements');
+            const data= await response.json();
+            setTasks(data)
+            // console.log(data);
+        } catch (error) {
+            console.log(error);
+        }
     }
+
+    const handleDelete = async (id)=>{
+        try {
+            /*const res =*/ await fetch(`http://localhost:4000/deleteelement/${id}`, {
+                method: 'DELETE'
+            });
+            // console.log(res);
+            setTasks(tasks.filter((task)=>task.id !== id))
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     useEffect(()=>{
         loadTasks()
     }, [])
+    
     return (
         <>
             <h1>
@@ -24,7 +43,9 @@ function TaskList() {
                         marginBottom: '.9rem',
                         backgroundColor: '#10556e',
                         color: '#ffffff'
-                    }}>
+                    }}
+                    key={task.id}
+                    >
                         <CardContent style={{
                             display: "flex",
                             justifyContent: "space-between"
@@ -47,7 +68,7 @@ function TaskList() {
                                 <Button 
                                 variant='outlined' 
                                 color='warning' 
-                                onClick={()=>console.log('delete')}
+                                onClick={()=>handleDelete(task.id)}
                                 style={{marginLeft:'.6rem'}}
                                 >
                                     Eliminar
